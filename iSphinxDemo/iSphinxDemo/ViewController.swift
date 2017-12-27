b85128d90b6b01e8ca70f1042dab2ced3a6bdc2e
Saiful Irham Wicaksana <iwicaksana@dyned.com>
2017-12-27 18:28:43 +0700

    update iSphinxDemo project

--- iSphinxDemo/iSphinxDemo/ViewController.swift
+++ iSphinxDemo/iSphinxDemo/ViewController.swift
@@ -11,43 +11,85 @@ import iSphinx
 
 class ViewController: UIViewController, iSphinxDelegete {
     
-    var isphinx: iSphinx = iSphinx()
+    @IBOutlet weak var txtVocabularies: UITextField!
+    @IBOutlet weak var txtWordDistractor: UITextField!
+    @IBOutlet weak var btnUpdateVocabularies: UIButton!
+    @IBOutlet weak var btnStartRecognizer: UIButton!
+    @IBOutlet weak var btnPlayAudio: UIButton!
+    @IBOutlet weak var lblUnsupportedWords: UILabel!
+    @IBOutlet weak var lblPartialResult: UILabel!
+    @IBOutlet weak var lblFinalResult: UILabel!
+    
+    fileprivate var isphinx: iSphinx = iSphinx()
+    fileprivate var indicator: UIActivityIndicatorView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        isphinx.delegete = self
-        isphinx.prepareISphinx(onPreExecute: { (config) in
+        self.setupProgress()
+        self.isphinx.delegete = self
+        self.indicator.startAnimating()
+        self.isphinx.prepareISphinx(onPreExecute: { (config) in
             
         }) { (isSuccess) in
-            
+            if isSuccess {
+                self.indicator.stopAnimating()
+            }
         }
-        
-        isphinx.updateVocabulary(text: "") {
-            
-        }
-        
-        isphinx.startISphinx(timeoutInSec: 10)
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    fileprivate func setupProgress() {
+        indicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+        indicator.frame = CGRect(x: 0.0, y: 0.0, width: 40, height: 40)
+        indicator.center = view.center
+        view.addSubview(indicator)
+        indicator.bringSubview(toFront: view)
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    @IBAction func btnUpdateVocabularyOnTouch(_ sender: Any) {
+        indicator.startAnimating()
+        isphinx.updateVocabulary(text: txtVocabularies.text!) {
+            self.indicator.stopAnimating()
+        }
+    }
+    
+    @IBAction func btnStartRecognizerOnTouch(_ sender: Any) {
+        isphinx.startISphinx(timeoutInSec: 10)
+    }
+    
+    @IBAction func btnPlayAudioOnTouch(_ sender: Any) {
+        
+    }
+    
 
     func iSphinxDidStop(reason: String, code: Int) {
-        
+        if code == 500 { // 500 code for error
+            print(reason)
+        } else if code == 522 { // 522 code for timed out
+            print(reason)
+        } else if code == 200 { // 200 code for finish speech
+            print(reason)
+        }
     }
     
     func iSphinxFinalResult(result: String, hypArr: [String], scores: [Double]) {
-        
+        lblFinalResult.text = result
     }
     
     func iSphinxPartialResult(partialResult: String) {
-        
+        lblPartialResult.text = partialResult
     }
     
     func iSphinxUnsupportedWords(words: [String]) {
-        
+        var unsuportedWords: String = ""
+        for word in words {
+            unsuportedWords += "\(word) "
+        }
+        lblUnsupportedWords.text = unsuportedWords
     }
     
     func iSphinxDidSpeechDetected() {
-        
+        print("Speech detected!")
     }
 
 }
